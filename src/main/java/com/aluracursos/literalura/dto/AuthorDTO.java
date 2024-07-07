bac7650bc03f9d6f5bf2f7cf6e123c0d3c2245a8
Octavio Benitez <tatokinneas@gmail.com>
2024-07-07 13:27:46 -0600

    Feat: DTO design pattern

--- src/main/java/com/aluracursos/literalura/dto/AuthorDTO.java
+++ src/main/java/com/aluracursos/literalura/dto/AuthorDTO.java
@@ -1,4 +1,12 @@
 package com.aluracursos.literalura.dto;
 
-public record AuthorDTO() {
+import com.aluracursos.literalura.model.Books;
+import jakarta.persistence.*;
+
+public record AuthorDTO(
+        String name,
+        String birth_year,
+        String death_year,
+        Books book
+) {
 }
--- src/main/java/com/aluracursos/literalura/dto/BookDTO.java
+++ src/main/java/com/aluracursos/literalura/dto/BookDTO.java
@@ -1,4 +1,14 @@
 package com.aluracursos.literalura.dto;
 
-public record BookDTO() {
+import com.aluracursos.literalura.model.Authors;
+import jakarta.persistence.*;
+
+import java.util.List;
+
+public record BookDTO(
+        String title,
+        List<String>languages,
+        Double downloads,
+        List<Authors>authorsList
+) {
 }
